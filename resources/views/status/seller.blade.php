e9914de69f28dfe2a026c352325d556e6c7da76c
Brian Rainer Suryaputra <brianrainer5497@gmail.com>
2018-05-16 11:03:29 +0700

    add confirmation modal for reject order

--- resources/views/status/seller.blade.php
+++ resources/views/status/seller.blade.php
@@ -32,13 +32,9 @@
                 "MENUNGGU")
                 <form method="POST" action="{{url('/pemesanan/toko/accept')}}">
                   {{csrf_field()}}  
+                  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#reject-confirmation" onclick="cancel('{{$item->id}}', '{{$item->order_id}}', '{{$item->user_name}}', '{{$item->menu_name}}', '{{$item->qty}}', '{{$item->order_item_status}}')">Tolak</button>
                   <input type="hidden" name="order_item_id" value="{{$item->id}}">
-                  <button type="submit" class="btn btn-default">Terima</button>  
-                </form>
-                <form method="POST" action="{{url('/pemesanan/toko/reject')}}">
-                  {{csrf_field()}}  
-                  <input type="hidden" name="order_item_id" value="{{$item->id}}">
-                  <button type="submit" class="btn btn-default">Tolak</button>  
+                  <button type="submit" class="btn btn-success">Terima</button>  
                 </form>
               @elseif($item->order_item_status == "MOHON TUKAR")
                 <form method="POST" action="{{url('/pemesanan/toko/close')}}">
@@ -63,4 +59,52 @@
       </tbody>
     </table>
   </div>
+
+  <div id="reject-confirmation" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <form class="modal-content" method="POST" action="{{url('/pemesanan/toko/reject')}}">
+        {{csrf_field()}}
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Konfirmasi Penolakan</h4>
+        </div>
+        <div class="modal-body">
+          <div id="order-table" class="table fixed-layout">
+          </div>
+          {{-- hidden input --}}
+          <div id="order-form"></div>
+          Apakah Anda yakin ingin menolak pesanan ini ?
+        </div>
+
+        <div class="modal-footer">
+          <div align="center">
+            <button type="button" class="btn btn-default">Batal</button>
+            <button type="submit" class="btn btn-danger">Tolak Pesanan</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+@endsection
+
+@section('js')
+  <script type="text/javascript">
+    function cancel(order_item_id, order_id, user_name, menu_name, qty, status){
+      console.log(order_item_id);
+      console.log(order_id);
+
+      var table = document.getElementById('order-table');
+      var form = document.getElementById('order-form');
+
+      table.innerHTML = 
+        '<div> No.     :  ' + order_id + '</div>' + 
+        '<div> Pemesan : ' + user_name + '</div>' + 
+        '<div> Pesanan : ' + menu_name + '</div>' + 
+        '<div> Jumlah  : ' + qty + '</div>' + 
+        '<div> Status  : ' + status + '</div>';
+
+      form.innerHTML = '<input type="hidden" name="order_item_id" value="'+order_item_id+'">';
+
+    }
+  </script>
 @endsection
